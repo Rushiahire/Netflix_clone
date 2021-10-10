7c5e392c56edf4c54cca8ed31f39aad16d06dae1
Rushiahire <rushiahire9567@gmail.com>
2021-10-10 16:52:31 -0500

    practice

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var q1=document.getElementById("q1");
+var q2=document.getElementById("q2");
+var q3=document.getElementById("q3");
+var q4=document.getElementById("q4");
+var q5=document.getElementById("q5");
+var q6=document.getElementById("q6");
+
+var faq_1=document.getElementById("faq_1");
+var faq_2=document.getElementById("faq_2");
+var faq_3=document.getElementById("faq_3");
+var faq_4=document.getElementById("faq_4");
+var faq_5=document.getElementById("faq_5");
+var faq_6=document.getElementById("faq_6");
+
+q6.addEventListener('click',function(){
+    faq_1.style.display="block";
+});
